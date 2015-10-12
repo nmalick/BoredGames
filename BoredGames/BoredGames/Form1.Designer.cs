6a2ff96dc7e58428a3ebed1f19284fab1564414d
Malick Ndiaye <emnww3@mst.edu>
2015-10-12 18:55:11 -0500

    First Draft
    Basic UI

---
+++ BoredGames/BoredGames/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BoredGames
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnSudoku = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSudoku
+            // 
+            this.btnSudoku.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnSudoku.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnSudoku.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSudoku.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSudoku.Location = new System.Drawing.Point(39, 196);
+            this.btnSudoku.Name = "btnSudoku";
+            this.btnSudoku.Size = new System.Drawing.Size(249, 148);
+            this.btnSudoku.TabIndex = 0;
+            this.btnSudoku.Text = "Sudoku";
+            this.btnSudoku.UseVisualStyleBackColor = false;
+            this.btnSudoku.Click += new System.EventHandler(this.btnSudoku_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExit.BackgroundImage")));
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnExit.ForeColor = System.Drawing.Color.Transparent;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(786, 223);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(249, 148);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1098, 644);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSudoku);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "#BoredGamez";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSudoku;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
